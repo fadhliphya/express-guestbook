e29e431349806a6e25a027a8f42c9d2b918c84ad
Maureen Ghassani Fadhliphya <maureenghassani@gmail.com>
2025-06-04 20:00:02 +0700

    Fixing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,4 @@
 const app = require('./app');
+const serverless = require('serverless-http');
 
-const HOST = process.env.HOST || '0.0.0.0';
-const PORT = process.env.PORT || 8080;
-
-app.listen(PORT, HOST);
-console.log(`Running on http://${HOST}:${PORT}`);
+module.exports = serverless(app);
