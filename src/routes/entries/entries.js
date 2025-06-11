b8ef71b640fdc475838338d4732137a7871cc53e
Maureen Ghassani Fadhliphya <maureenghassani@gmail.com>
2025-06-11 20:14:36 +0700

    Adding Search Bar Features

--- src/routes/entries/entries.js
+++ src/routes/entries/entries.js
@@ -1,15 +1,47 @@
 const express = require('express');
 const router = express.Router();
-
 const Entry = require('../../models/Entry');
 
+// Route default: tampilkan semua entri
 router.get('/', (req, res) => {
   Entry.find({})
     .then(entries => {
       entries.reverse();
-      res.status(200).render('pages/entries', { entries, error: null });
+      res.status(200).render('pages/entries/index', {
+        entries,
+        error: null,
+        keyword: null
+      });
     })
-    .catch(error => res.status(400).render('entries', { entries: [], error }));
+    .catch(error => res.status(400).render('pages/entries/index', {
+      entries: [],
+      error,
+      keyword: null
+    }));
+});
+
+// Route pencarian
+router.get('/search', (req, res) => {
+  const keyword = req.query.keyword;
+  Entry.find({
+    $or: [
+      { name: { $regex: keyword, $options: 'i' } },
+      { comment: { $regex: keyword, $options: 'i' } }
+    ]
+  })
+    .then(entries => {
+      entries.reverse();
+      res.status(200).render('pages/entries/index', {
+        entries,
+        keyword,
+        error: null
+      });
+    })
+    .catch(error => res.status(400).render('pages/entries/index', {
+      entries: [],
+      keyword,
+      error
+    }));
 });
 
 module.exports = router;
